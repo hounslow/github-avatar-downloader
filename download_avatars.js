a04f60500a98790d5d14ad9022ac50a2c3ad8722
hounslow <hounslowmatthew@gmail.com>
2017-08-01 02:10:34 +0000

    Cleaned up the code, images are going to an avatars folder

--- download_avatars.js
+++ download_avatars.js
@@ -5,42 +5,52 @@ var GITHUB_TOKEN = "1de2a7211489836dfc3111d0eba924c24b396abd";
 
 //console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getURL(){
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  var options = {
+      url: requestURL,
+        headers: {
+              'User-Agent': 'hounslow'
+      }
+  };
+  return options;
+}
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-var options = {
-    url: requestURL,
-      headers: {
-            'User-Agent': 'hounslow'
-    }
-};
-
-request(options, function (error, response, body) {
-  console.log('error:', error);
-  console.log('statusCode:', response.statusCode);
-  var object = JSON.parse(body);
-  object.forEach(function(user){
-    cb(user.avatar_url, user.login);
+  request(getURL(), function (error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response.statusCode);
+    var object = JSON.parse(body);
+    object.forEach(function(user){
+      cb(user.avatar_url, user.login);
+      });
     });
-  });
 }
 
 function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-                throw err;
-                })
-        .on('response', function (response) {
-          console.log('Response Status Code: ', response.statusCode);
-          })
-        .pipe(fs.createWriteStream('./' + filePath + '.jpg'));
+  request.get(url).on('error', function (err) {
+    throw err;
+    })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode);
+    })
+  .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
 }
 
-var owner = process.argv[2];
-var name = process.argv[3];
-if (owner && name){
-  getRepoContributors(owner, name, downloadImageByURL);
-} else {
-  console.log("Missing an argument!");
-  console.log("Please enter your request with following structure: node download_avatars.js <repoOwner> <repoName>");
-}
+// This calls getRepoContributors, which will get the response, error and body and then call downloadImageByURL to save the images to the avatar folder
+var callDownloader = function(){
+
+  repoOwner = process.argv[2];
+  repoName = process.argv[3];
+
+// Log an error if owner and name aren't included in command line
+  if (repoOwner && repoName){
+    getRepoContributors(repoOwner, repoName, downloadImageByURL);
+  } else {
+    console.log("Missing an argument!");
+    console.log("Please enter your request with following structure: node download_avatars.js <repoOwner> <repoName>");
+  }
+}();
