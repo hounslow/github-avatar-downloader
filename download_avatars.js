e25ec86922c7a1e644495ad30305f79b8a1f9393
hounslow <hounslowmatthew@gmail.com>
2017-08-01 00:31:26 +0000

    requestURL works, User-Agent needed

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,17 @@
 var request = require('request');
+var GITHUB_USER = "hounslow";
+var GITHUB_TOKEN = "1de2a7211489836dfc3111d0eba924c24b396abd";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  request.get('https://api.github.com/repos/'+ repoOwner + '/' + repoName + '/contributors')
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+request.get(requestURL)
   .on('error', function (err) {
-                throw err;
-                })
-        .on('response', function (response) {
+             throw err;
+              })
+  .on('response', function (response) {
           console.log('Response Status Code: ', response.statusCode);
           });
 }
