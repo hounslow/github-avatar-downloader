a20b463b4a8963449869b91e95c50f48a42b2dec
hounslow <hounslowmatthew@gmail.com>
2017-08-01 01:22:25 +0000

    Getting avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -2,18 +2,36 @@ var request = require('request');
 var GITHUB_USER = "hounslow";
 var GITHUB_TOKEN = "1de2a7211489836dfc3111d0eba924c24b396abd";
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+//console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-request.get(requestURL)
-  .on('error', function (err) {
-             throw err;
-              })
-  .on('response', function (response) {
-          console.log('Response Status Code: ', response.statusCode);
-          });
+var options = {
+    url: requestURL,
+      headers: {
+            'User-Agent': 'hounslow'
+    }
+};
+
+//request.get(options)
+ // .on('error', function (err) {
+   //          throw err;
+     //         })
+  //.on('response', function (response) {
+    //      console.log('Response Status Code: ', response.statusCode);
+     //     })
+  //.on('body', function(body){
+   // console.log(body);
+    //});
+request(options, function (error, response, body) {
+  console.log('error:', error);
+  console.log('statusCode:', response.statusCode);
+  var object = JSON.parse(body);
+  object.forEach(function(user){
+    console.log(user['avatar_url']);
+    });
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
